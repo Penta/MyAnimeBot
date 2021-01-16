c7d468aef149277e85295d6f3a7f713813ea9de6
Penta <andy.esnard@gmail.com>
2021-01-16 03:19:51 +0100

    Fixing non-working logging function

--- myanimebot.py
+++ myanimebot.py
@@ -419,6 +419,6 @@ if __name__ == "__main__":
 	try:
 		globals.client.run(globals.token)
 	except Exception as e:
-		globals.logger.logging.error("Encountered exception while running the bot: {}".format(e))
+		globals.logger.error("Encountered exception while running the bot: {}".format(e))
 
 	exit_app()
